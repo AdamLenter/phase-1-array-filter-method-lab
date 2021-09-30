e06557ea2282f5bf0fd8cc483897a006a7640169
Adam <adamlenter1@gmail.com>
2021-09-30 15:12:06 -0400

    commit tree 755a5a929e56ca82f08efe8aaafecd1ca52f5a02

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const drivers = ["John", "Fred", "Tom", "John"];
+
+function findMatching(listOfDrivers, nameToMatch) {
+    const filteredList = listOfDrivers.filter(function (driver) {
+        return driver.toUpperCase() == nameToMatch.toUpperCase();
+      });
+      return filteredList;
+}
+
+function fuzzyMatch(listOfDrivers, nameToMatch)
+    {
+    const nameLength = nameToMatch.length;
+    const filteredList = listOfDrivers.filter(function (driver) {
+        return driver.toUpperCase().substring(0, nameLength) == nameToMatch.toUpperCase().substring(0, nameLength);
+        });
+    return filteredList; 
+    }
